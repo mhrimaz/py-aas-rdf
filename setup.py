0faa46d92c1eed8b7a6c78287f550a0da7ea2455
Hossein Rimaz <mhrimaz@yahoo.com>
2025-01-26 23:40:33 +0100

    Modify dependency version
    restricting the pydantic version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['pydantic>=2', 'rdflib>=7', ]
+requirements = ['pydantic>=2.0,<2.2', 'rdflib>=7', ]
 
 test_requirements = ['pytest>=3', ]
 
